3398dfdaed97171b06bd9fa467e18dcfa1de3c49
pakornOliva <pakornoliva>
2015-02-23 23:36:58 -0600

    Cambios- 23-02-2015 - ksa

---
+++ contacto.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <meta charset="utf-8">
+  <title>Did&aacute;ctica | M&aacute;xima Aventura al Viajar</title>
+  <link href="styles/960.css" rel="stylesheet" type="text/css" />
+  <link href="styles/reset.css" rel="stylesheet" type="text/css" />
+  <link href="styles/text.css" rel="stylesheet" type="text/css" />
+  <link href="styles/estilos.css" rel="stylesheet" type="text/css" />
+  <link href="styles/slider.css" rel="stylesheet" type="text/css" />
+  <link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+  <div class="wrapper">
+    <div class="container_12">
+      <div class="logo"></div>
+      <div class="nav-menu">
+        <span class="menu">
+          <ul>
+            <li><a class="menu-item1" href="index.html">INICIO</a></li>						
+            <li><a class="menu-item2 active" href="porque-elegirnos.html">POR QUÉ ELEGIRNOS?</a></li>
+            <li><a class="menu-item3" href="destinos.html">DESTINOS</a></li>
+            <li><a class="menu-item4" href="#">GALERIA</a></li>
+            <li><a class="menu-item5" href="#">CONTACTO</a></li>						
+          </ul>
+        </span>
+      </div>
+    </div>
+    <div class="container_12 main-content ">
+<form name="contactform" method="post" action="send_form_email.php"> 
+<table width="450px"><!---Este tamaño en px es personalizable -->
+<tr>
+ <td valign="top">
+  <label for="first_name">Nombre *</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="first_name" maxlength="50" size="30">
+ </td>
+</tr>
+<tr>
+ <td valign="top">
+  <label for="last_name">Apellido *</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="last_name" maxlength="50" size="30">
+ </td>
+</tr>
+<tr>
+ <td valign="top">
+  <label for="email">E-mail *</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="email" maxlength="80" size="30">
+ </td>
+</tr>
+<tr>
+ <td valign="top">
+  <label for="telephone">Teléfono</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="telephone" maxlength="30" size="30">
+ </td>
+</tr>
+<tr>
+ <td valign="top">
+  <label for="message">Mensaje *</label>
+ </td>
+ <td valign="top">
+  <textarea  name="message" maxlength="1000" cols="25" rows="6"></textarea>
+ </td>
+</tr>
+<tr>
+ <td colspan="2" style="text-align:center">
+  <input type="submit" value="Submit">   <a href="http://www.ladirecciondetusitio/email_form.php">Enviar</a>
+ </td>
+</tr>
+</table>
+</form>
+    
+			      
+    </div>
+  <!--main-content-->
+  </div>
+  <!--wrapper-->
+  <div class="footer container_12">
+    <div class="footer-content">
+      <span>Didáctica &copy; Derechos Reservados 2014.</span><a href="aviso-privacidad.html">Politica de Privacidad</a>
+    </div>
+  </div>
+</body>
+
+</html>
